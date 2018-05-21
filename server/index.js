60e4a7d9ce545de3fb451c1b225f0e2a9ca0bdca
chrisekelley <chris@vetula.com>
2018-05-21 14:18:54 +0200

    added uuid package for release script
    better logging on receiveMessage

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 const RPC = require('./rpc')
 const idb = require('random-access-idb')
 
-console.log("Making RPC global.")
+console.log("Making RPC and idb global.")
 window.RPC = RPC;
 window.idb = idb;
-
-//
 
